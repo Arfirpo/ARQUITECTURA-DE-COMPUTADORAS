7b78adb7fceba949938e6a80a578e2b070b87771
Agustin Firpo <agustin_@outlook.com.ar>
2025-10-28 20:05:32 -0300

    practica 4 - parte 1 - ejercicio 5b - en proceso

--- WinMips-Practicas-4-a-6/Practica-4/P4-p1-EJ5b.s
+++ WinMips-Practicas-4-a-6/Practica-4/P4-p1-EJ5b.s
@@ -2,31 +2,31 @@
 A:  .word   4
 B:  .word   2
 c:  .word   0
-P:  .word   0
-D:  .word   0
 
 
     .code
     ld      $t0,    A($0)
     ld      $t1,    B($0)
-    ld      $t2,    c($0)
 
-    slti    $t3,    $t0,        0
-    beqz    $t3,    esIgual
+
+    beq     $t0,    $0,             esIgual
+
+    slt     $t3,    $t1,            $t0     ; comparo si (B < A) --> true = 1, false = 0
+    beqz    $t3,    mayorOigual
+
+    daddi   $t5,    $0,             2
+    dmul    $t5,    $t0,            $t5
+    sd      $t5,    C($0)
+
+mayorOigual:
+    sd      $t1,    C($0)
+    j       fin
 
 esIgual:
-    daddi   $t4,    $0,         0
+    daddi   $t4,    $0,             0
     sd      $t4,    c($0)
-
-    slt     $t3, $t1, $t0
-    bnez    $t3  
-
 fin:
     halt
-
-
-
-
 
 
 
