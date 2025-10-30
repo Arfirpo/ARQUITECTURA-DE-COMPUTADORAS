44fa68dfa7df8ed1168e63f01d9ca0b8a2cb5cea
Agustin Firpo <agustin_@outlook.com.ar>
2025-10-30 18:02:32 -0300

    practica 4 - parte 1 - ejercicio 5c - completo

--- WinMips-Practicas-4-a-6/Practica-4/P4-p1-EJ5b.s
+++ WinMips-Practicas-4-a-6/Practica-4/P4-p1-EJ5b.s
@@ -1,7 +1,7 @@
 .data
 A:  .word   4
 B:  .word   2
-c:  .word   0
+C:  .word   0
 
 
     .code
@@ -9,22 +9,22 @@ c:  .word   0
     ld      $t1,    B($0)
 
 
-    beq     $t0,    $0,             esIgual
+    beqz     $t0,    esIgual
 
     slt     $t3,    $t1,            $t0     ; comparo si (B < A) --> true = 1, false = 0
-    beqz    $t3,    mayorOigual
+    bnez    $t3,    mayorOigual
 
     daddi   $t5,    $0,             2
-    dmul    $t5,    $t0,            $t5
-    sd      $t5,    C($0)
+    dmul    $t6,    $t0,            $t5
+    sd      $t6,    C($0)
+    j       fin
 
 mayorOigual:
     sd      $t1,    C($0)
     j       fin
 
 esIgual:
-    daddi   $t4,    $0,             0
-    sd      $t4,    c($0)
+    sd      $0,    C($0)
 fin:
     halt
 
