b9a70b758d3adb0a68a8d6e04fcb2464700ba5cd
Agustin Firpo <agustin_@outlook.com.ar>
2025-11-04 20:08:34 -0300

    practica 4 - parte 1 - ejercicio 6 - completo

--- WinMips-Practicas-4-a-6/Practica-4/P4-P1-EJ6.s
+++ WinMips-Practicas-4-a-6/Practica-4/P4-P1-EJ6.s
@@ -45,28 +45,23 @@ seguir:
 
     ;--------------------------------------------
 
-    ;C)
-
+    ;D)
 
 .data
-V:  .word   5, 2, 6
-R:  .word   0
+V:  .word32   5, 2, 6
+R:  .word32   0
 
     .code
 
     daddi   $t0,    $0,     0   ;resultado
     daddi   $t1,    $0,     3   ;iteraciones
-
-    daddi   $t2,    $0,     V   ;desplazamiento
-
+    daddi   $t2,    $0,     0   ;desplazamiento
 seguir:
-    ld      $t3,    0($t2)
+    lw      $t3,    V($t2)
     dadd    $t0,    $t0,    $t3
     daddi   $t2,    $t2,    4
     daddi   $t1,    $t1,    -1
     bnez    $t1,    seguir
-fin:
-
-    sd      $t0,    R($0)
+    sw      $t0,    R($0)
     halt
 
